cddd27247548f46141bdcaf820726a667d74c414
enilu <82552623@qq.com>
2015-03-16 17:53:33 +0800

    启动主类

---
+++ src/main/java/com/cnendata/dev/collector/Startup.java
@@ -0,0 +1,33 @@
+/**
+ *Startup.java
+ *Version1.0
+ *2015-3-16
+ *Copyright cnendata.com
+ *
+ */
+package com.cnendata.dev.collector;
+
+/**
+ * 采集器启动类<br>
+ * <!--<br>
+ * 历史记录：<br>
+ * --------------------------------------------------------
+ * 2015-3-16,enilu(82552623@qq.com)新建文档<br>
+ * 
+ * -->
+ * 
+ * @author enilu(82552623@qq.com)
+ * 
+ *         since1.0
+ */
+public class Startup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("start up...");
+
+	}
+
+}
