99283445b362c4fbffdf86e78c6d75b01a622937
enilu <82552623@qq.com>
2015-03-20 10:01:56 +0800

    TestAll

---
+++ src/test/java/com/cnendata/dev/TestAll.java
@@ -0,0 +1,30 @@
+/**
+ *TestAll.java
+ *Version1.0
+ *2015-3-20
+ *Copyright cnendata.com
+ *
+ */
+package com.cnendata.dev;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * descript<br>
+ * <!--<br>
+ * 历史记录：<br>
+ * --------------------------------------------------------
+ * 2015-3-20,enilu(82552623@qq.com)新建文档<br>
+ * 
+ * -->
+ * 
+ * @author enilu(82552623@qq.com)
+ * 
+ *         since1.0
+ */
+@RunWith(Suite.class)
+// @Suite.SuiteClasses({ AllUtil.class, AllJms.class, AllParser.class,
+// AllQueue.class, AllTask.class, AllThreadPool.class, AllWebsite.class })
+public class TestAll {
+}
